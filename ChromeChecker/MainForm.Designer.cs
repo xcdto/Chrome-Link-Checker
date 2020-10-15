c9905e45bac2f08b2a6cd14fafa5f466c0352d24
Hacker-TA <51052767+Hacker-TA@users.noreply.github.com>
2020-10-15 16:03:42 +0300

    ChromeChecker

---
+++ ChromeChecker/MainForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ChromeChecker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.checker = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checker
+            // 
+            this.checker.Enabled = true;
+            this.checker.Interval = 4000;
+            this.checker.Tick += new System.EventHandler(this.checker_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(282, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Your open page in the browser:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 32);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(322, 71);
+            this.MinimumSize = new System.Drawing.Size(322, 71);
+            this.Name = "MainForm";
+            this.Text = "Checker";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer checker;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ChromeChecker/MainForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+using System.Windows.Forms;
+
+namespace ChromeChecker
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public static string GetChromeUrl(Process process)
+        {
+            if (process == null)
+                throw new ArgumentNullException("process");
+
+            if (process.MainWindowHandle == IntPtr.Zero)
+                return null;
+
+            AutomationElement element = AutomationElement.FromHandle(process.MainWindowHandle);
+            if (element == null)
+                return null;
+
+            AutomationElementCollection edits5 = element.FindAll(TreeScope.Subtree, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Edit));
+            AutomationElement edit = edits5[0];
+            string url = ((ValuePattern)edit.GetCurrentPattern(ValuePattern.Pattern)).Current.Value as string;
+            return url;
+        }
+
+        private void checker_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (Process process in Process.GetProcessesByName("chrome"))
+                {
+                    string url = GetChromeUrl(process);
+                    if (url == null)
+                        continue;
+                    if (url == "")
+                        label1.Text = "Your open page in the browser: " + "none";
+                    else
+                        label1.Text = "Your open page in the browser: " + url;
+                }
+            }
+            catch
+            {
+                label1.Text = "Your open page in the browser: " + "none";
+            }
+
+        }
+    }
+}
